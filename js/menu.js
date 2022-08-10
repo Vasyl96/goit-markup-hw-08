0013957dd334393e7e0347318beca86e31c88403
Vasyl Horaichuk <vg.createam@gmail.com>
2022-08-10 14:25:48 +0300

    Виконав деякі правки

---
+++ js/menu.js
@@ -0,0 +1,16 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-menu-open]"),
+    closeModalBtn: document.querySelector("[data-menu-close]"),
+    modal: document.querySelector("[data-menu]"),
+    body: document.querySelector("[data-page]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-open");
+    refs.body.classList.toggle("no-scroll");
+  }
+})();
